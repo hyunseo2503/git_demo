dbf74600bb4f5142eb965bc95e09d986c848c755
hyunseo-lee <hyunseo2503@gmail.com>
2026-04-24 10:40:09 +0900

    1차 릴리즈

---
+++ src/main/java/com/example/git_demo/GitDemo1.java
@@ -0,0 +1,18 @@
+package com.example.git_demo;
+
+public class GitDemo1 {
+    public static void main(String[] args) {
+    }
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public String getStatus() {
+        return "Application is running";
+    }
+
+}
